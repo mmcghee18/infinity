5c378395a0046a075e2df776a74ee8006b4d2cab
Michelle <michmac202@gmail.com>
2020-07-15 12:25:17 -0500

    Updates

---
+++ precache-manifest.2cf340f912b77500eabb7f6aa0dad240.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24bc33213a44032ab478cadae56451e6",
+    "url": "/infinity/index.html"
+  },
+  {
+    "revision": "c53b2beeaffde2db11a2",
+    "url": "/infinity/static/js/2.c2c759ea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/infinity/static/js/2.c2c759ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e14dab242e7c1f6ec44d",
+    "url": "/infinity/static/js/main.1da4db02.chunk.js"
+  },
+  {
+    "revision": "9b3ecd0bafd0718b7e6c",
+    "url": "/infinity/static/js/runtime-main.d9a80ca9.js"
+  }
+]);
